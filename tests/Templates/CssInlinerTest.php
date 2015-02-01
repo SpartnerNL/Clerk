8b6d74e277f374b9127002a9813b72976eba0420
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2015-02-01 12:43:12 +0100

    Stylesheet extract and css inliner tests

---
+++ tests/Templates/CssInlinerTest.php
@@ -0,0 +1,37 @@
+<?php namespace Maatwebsite\Clerk\Tests\Templates;
+
+use Maatwebsite\Clerk\Templates\Css\CssInliner;
+
+class CssInlinerTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_inline_style_blocks_get_inlined()
+    {
+        $html = '<style>
+            table {
+                background: #000000;
+            }
+        </style>
+        <table></table>';
+
+        $inliner = new CssInliner();
+        $inlined = $inliner->transformCssToInlineStyles($html);
+
+        $this->assertEquals('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html><head><style></style></head><body><table style="background: #000000;"></table></body></html>
+', $inlined);
+    }
+
+
+    public function test_css_from_style_sheets_links_get_inlined()
+    {
+        $html = '<link href="' . __DIR__ . '/css/' . 'style.css" rel="stylesheet">
+        <table></table>';
+
+        $inliner = new CssInliner();
+        $inlined = $inliner->transformCssToInlineStyles($html);
+
+        $this->assertEquals('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html><head><link href="' . __DIR__ . '/css/' . 'style.css" rel="stylesheet"></head><body><table style="background: #000000;"></table></body></html>
+', $inlined);
+    }
+}
---
+++ tests/Templates/StylesheetExtractorTest.php
@@ -0,0 +1,28 @@
+<?php namespace Maatwebsite\Clerk\Tests\Templates;
+
+use Maatwebsite\Clerk\Templates\Css\StylesheetExtractor;
+
+class StylesheetExtractorTest extends \PHPUnit_Framework_TestCase {
+
+
+    public function test_extracting_stylesheets_from_html()
+    {
+        $html = '<link href="' . __DIR__ . '/css/' . 'style.css" rel="stylesheet"><link href="' . __DIR__ . '/css/' . 'style1.css" rel="stylesheet"><link href="' . __DIR__ . '/css/' . 'style2.css" rel="stylesheet">';
+
+        $extractor = new StylesheetExtractor($html);
+        $result = $extractor->extract();
+
+        $this->assertCount(3, $result);
+    }
+
+
+    public function test_extracting_stylesheets_returns_the_css()
+    {
+        $html = '<link href="' . __DIR__ . '/css/' . 'style.css" rel="stylesheet">';
+
+        $extractor = new StylesheetExtractor($html);
+        $result = $extractor->extract();
+
+        $this->assertEquals('table { background: #000000; }', $result[__DIR__ . '/css/' . 'style.css']);
+    }
+}
